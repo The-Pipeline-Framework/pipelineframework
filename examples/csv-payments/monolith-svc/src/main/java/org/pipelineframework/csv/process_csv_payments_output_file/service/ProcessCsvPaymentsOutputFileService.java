d0ea17b71c041b1546a60e1b18708da3bb9a74ad
mariano.barcia <mariano.barcia@gmail.com>
2026-02-17 03:48:50 +0100

    Fix monolith step duplication and service name collision

--- examples/csv-payments/monolith-svc/src/main/java/org/pipelineframework/csv/process_csv_payments_output_file/service/ProcessCsvPaymentsOutputFileService.java
+++
@@ -1,7 +0,0 @@
-package org.pipelineframework.csv.process_csv_payments_output_file.service;
-
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class ProcessCsvPaymentsOutputFileService extends org.pipelineframework.csv.service.ProcessCsvPaymentsOutputFileService {
-}
