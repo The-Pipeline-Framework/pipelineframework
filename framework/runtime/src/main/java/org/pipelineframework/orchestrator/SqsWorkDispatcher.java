61df741271267292d5adf996b4e5fdfe5c945f25
mariano.barcia <mariano.barcia@gmail.com>
2026-03-10 01:38:06 +0100

    Fix native image Random heap failures in AWS clients

--- framework/runtime/src/main/java/org/pipelineframework/orchestrator/SqsWorkDispatcher.java
+++ framework/runtime/src/main/java/org/pipelineframework/orchestrator/SqsWorkDispatcher.java
@@ -15,6 +15,7 @@ import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.infrastructure.Infrastructure;
 import org.jboss.logging.Logger;
 import org.pipelineframework.config.pipeline.PipelineJson;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
@@ -130,6 +131,7 @@ public class SqsWorkDispatcher implements WorkDispatcher {
         synchronized (this) {
             if (client == null) {
                 var builder = SqsClient.builder();
+                builder.httpClientBuilder(UrlConnectionHttpClient.builder());
                 orchestratorConfig.sqs().region()
                     .filter(region -> !region.isBlank())
                     .ifPresent(region -> builder.region(Region.of(region)));
