535394ca24cb25a78e6188eeaab188a98adc22d0
Mariano Barcia <mariano.barcia@gmail.com>
2026-04-12 16:47:32 +0200

    Update quotation marks in devto-feed.js

--- docs/functions/api/devto-feed.js
+++ docs/functions/api/devto-feed.js
@@ -23,10 +23,10 @@ const XML_ENTITY_MAP = {
   mdash: '—',
   ndash: '–',
   hellip: '…',
-  rsquo: ''',
-  lsquo: ''',
-  rdquo: '"',
-  ldquo: '"'
+  rsquo: '’',
+  lsquo: '‘',
+  rdquo: '”',
+  ldquo: '“'
 }
 
 const parseLimit = (value) => {
