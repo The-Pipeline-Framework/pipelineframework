1947ab925ed92ca2aa8f28e6a8d0c892d06517a6
mariano.barcia <mariano.barcia@gmail.com>
2026-06-03 13:36:21 +0200

    Fix csv-payments image build regression

--- examples/csv-payments/build-modular-telemetry-images.sh
+++ examples/csv-payments/build-modular-telemetry-images.sh
@@ -35,10 +35,23 @@ cp "$MODULAR_MAPPING" "$ACTIVE_MAPPING"
 cd "$ROOT_DIR"
 
 IMAGE_TAG="${CSV_E2E_TELEMETRY_IMAGE_TAG:-otel}"
+case "$(uname -m)" in
+  x86_64|amd64)
+    DEFAULT_IMAGE_PLATFORMS="linux/amd64"
+    ;;
+  arm64|aarch64)
+    DEFAULT_IMAGE_PLATFORMS="linux/arm64/v8"
+    ;;
+  *)
+    DEFAULT_IMAGE_PLATFORMS="linux/amd64"
+    ;;
+esac
+IMAGE_PLATFORMS="${CSV_E2E_IMAGE_PLATFORMS:-$DEFAULT_IMAGE_PLATFORMS}"
 
 ./mvnw -f examples/csv-payments/pom.xml -DskipTests clean package \
   -Dtpf.build.transport=GRPC \
   -Dquarkus.container-image.tag="${IMAGE_TAG}" \
+  -Dquarkus.container-image.platforms="${IMAGE_PLATFORMS}" \
   -Dquarkus.otel.enabled=true \
   -Dquarkus.otel.sdk.disabled=false \
   -Dquarkus.otel.metrics.enabled=false \
