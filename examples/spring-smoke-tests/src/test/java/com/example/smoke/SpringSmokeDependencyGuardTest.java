369165f88fd0fdae426217a3c5f6844743c9ec19
mariano.barcia <mariano.barcia@gmail.com>
2026-09-12 17:34:02 +0200

    test(spring): guard Quarkus runtime classpath

--- examples/spring-smoke-tests/src/test/java/com/example/smoke/SpringSmokeDependencyGuardTest.java
+++ examples/spring-smoke-tests/src/test/java/com/example/smoke/SpringSmokeDependencyGuardTest.java
@@ -24,7 +24,7 @@ class SpringSmokeDependencyGuardTest {
     void springSmokeDoesNotExposeQuarkusRuntimeApi() {
         assertThrows(
             ClassNotFoundException.class,
-            () -> Class.forName("org.pipelineframework.service.ReactiveService"));
+            () -> Class.forName("org.pipelineframework.PipelineExecutionService"));
     }
 
     @Test
