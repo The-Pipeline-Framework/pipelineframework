58090d05a2b41ffd1086fcdc4415e7fa7e7ff17a
mariano.barcia <mariano.barcia@gmail.com>
2026-09-16 10:11:47 +0200

    chore: remove runtime-core connector mirror

--- framework/runtime-core/src/main/java/org/pipelineframework/connector/BlockingOperation.java
+++
@@ -1,7 +0,0 @@
-package org.pipelineframework.connector;
-
-/**
- * Operation whose invocation may block and must therefore begin on a framework worker.
- */
-public interface BlockingOperation extends ConnectorOperation {
-}
--- framework/runtime-core/src/main/java/org/pipelineframework/connector/QueryObservationOrigin.java
+++
@@ -1,7 +0,0 @@
-package org.pipelineframework.connector;
-
-/** Whether Query observation metadata came from a provider call or a capture replay. */
-public enum QueryObservationOrigin {
-    LIVE_PROVIDER,
-    CAPTURE_REPLAY
-}
