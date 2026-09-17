f5fd007a12744bbe33fe74122d89132d5b020386
mariano.barcia <mariano.barcia@gmail.com>
2026-09-17 12:42:45 +0200

    build: consume DSL and representation contracts externally

--- framework/representation-provider-api/src/main/java/org/pipelineframework/representation/spi/OperationRepresentationRole.java
+++
@@ -1,6 +0,0 @@
-package org.pipelineframework.representation.spi;
-
-/** Direction of one canonical/connector-wire representation boundary. */
-public enum OperationRepresentationRole {
-    REQUEST, RESPONSE, CALLBACK
-}
--- framework/representation-provider-api/src/main/java/org/pipelineframework/representation/spi/RepresentationScope.java
+++
@@ -1,7 +0,0 @@
-package org.pipelineframework.representation.spi;
-
-/** Configuration scope understood by the representation-provider lifecycle. */
-public enum RepresentationScope {
-    GLOBAL,
-    TYPE
-}
