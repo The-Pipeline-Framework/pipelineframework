fa7a1379cfb78501a3b7da852898160d1e9b2409
Mariano Barcia <mariano.barcia@gmail.com>
2026-09-17 21:07:48 +0200

    refactor: remove GraphQL connector source mirrors (#911)

--- framework/connectors/graphql-smallrye-connector/src/main/java/org/pipelineframework/connector/graphql/smallrye/GraphQlOperationKind.java
+++
@@ -1,7 +0,0 @@
-package org.pipelineframework.connector.graphql.smallrye;
-
-/** Static authority classification for an application-owned persisted operation. */
-public enum GraphQlOperationKind {
-    QUERY,
-    MUTATION
-}
