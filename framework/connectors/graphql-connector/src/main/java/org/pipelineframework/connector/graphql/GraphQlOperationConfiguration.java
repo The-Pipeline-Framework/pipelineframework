209ab1b76526f4fffc9848ba3f466bfb42950df2
mariano.barcia <mariano.barcia@gmail.com>
2026-09-17 20:47:32 +0200

    refactor: remove GraphQL connector source mirrors

--- framework/connectors/graphql-connector/src/main/java/org/pipelineframework/connector/graphql/GraphQlOperationConfiguration.java
+++
@@ -1,5 +0,0 @@
-package org.pipelineframework.connector.graphql;
-
-/** Empty typed operation configuration: persisted documents remain binding-owned. */
-public record GraphQlOperationConfiguration() {
-}
